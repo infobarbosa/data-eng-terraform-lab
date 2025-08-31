13c6747d2376710d1f3cc1eae921ba710cec070d
Barbosa <infobarbosa@yahoo.com.br>
2025-08-30 23:33:49 -0300

    adiciona variáveis para configuração da AWS no arquivo variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+    description = "The AWS region to deploy resources"
+    default     = "us-east-1"
+}
+
+
+variable "aws_access_key" {
+    description = "The AWS access key"
+    default     = "COLOQUE_SUA_CHAVE_DE_ACESSO_AQUI"
+}
+
+variable "aws_secret_key" {
+    description = "The AWS secret key"
+    default     = "COLOQUE_SUA_CHAVE_SECRETA_AQUI"
+}
